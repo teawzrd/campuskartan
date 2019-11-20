2b620369afc81a98ff6cbb8f5868ad2fb8d608f1
mieanda <57438528+mieanda@users.noreply.github.com>
2019-11-20 17:27:27 +0100

    Add files via upload

--- App.js
+++ App.js
@@ -9,7 +9,9 @@ import TP4 from './temp_kartor/TP41.png';
 import TP5 from './temp_kartor/TP51.png';
 
 
-import K from './temp_kartor/K4.png';
+import K2 from './temp_kartor/K_2.svg';
+import T3 from './temp_kartor/TP_3.svg';
+
 import overview from './temp_kartor/overview.jpg';
 import up_button from './symbols/up_arrow.png';
 import down_button from './symbols/down_arrow.png';
@@ -45,20 +47,6 @@ function Home() {
       setSearchString(event.target.value);
 
   }
- //max kod ang togglemenu
-  const [menuIsOpen, setMenuState] = useState(false);
-
-  const toggleMenu = event =>
-  {
-    if(menuIsOpen)
-    {
-      setMenuState(false);
-    }
-    else if(!menuIsOpen)
-    {
-      setMenuState(true);
-    }
-  };
 
   //function
   //hittar match beroende på sökord
@@ -77,21 +65,22 @@ var foundRooms = [];
 
   return (
     <div>
-      <div className = "background">
-        <img src={overview} className = "background_image"/>
-      </div>
       <div>
+      <div className = "topbar"></div>
       {/*sökruta*/}
       <div className = "searchbar">
-        <input type="text"  placeholder="Sök en sal.. " onChange={changeInput} onClick={toggleMenu}/>
+        <input type="text"  placeholder="Sök en sal.. " onChange={changeInput}/>
       </div>
         <div id= "listElement">
       {/*listar sökresultat fint*/}
+        <div id ="listBox">
           {foundRooms.slice(0,5).map(c => (<RoomInfo data={c} key={c.room}/>))}
-        
         </div>
-        <div style={{backgroundColor: "#205da2",
-        height: "10px"}} />
+        </div>
+        <div className = "background">
+        <img src={overview} className = "background_image"/>
+        </div>
+        
       </div>
     </div>
   );
@@ -99,7 +88,7 @@ var foundRooms = [];
 //varje rad i dropdown  + props.data.house
 function RoomInfo(props){
   return (
-    <Link to = {"/Täppan" }>
+    <Link className = "aa" to = {"/Täppan" }>
     <div className="listOverlay">
     {props.data.room}
     </div>
@@ -112,13 +101,16 @@ function Tappan() {
 	let top_floor = 5;
 	let current_floor = bottom_floor;
 
+  const [floor, setFloor] = useState(3);
+  console.log(floor);
+
 	//Kallar på egen function för knappar samt bakgrund
 	if(current_floor == bottom_floor) {
 		return (
 			<div>
-			<Buttons />
+			<Buttons floor={floor} setFloor={setFloor}/>
 			<div className = "background">
-				<img src={TP} className = "background_image"/>
+				<img src={T3} className = "background_image"/>
 			</div>
     </div>
   	);
@@ -157,7 +149,7 @@ function Kakenhus() {
 	<div>
 		<Buttons floor={floor} setFloor={setFloor}/>
 		<div className = "background">
-			<img src={K} className = "background_image"/>
+			<img src={K2} className = "background_image"/>
 		</div>
     </div>
   );
