6a0c2a0fd0138172af0e207da91d303adbdd169d
mieanda <57438528+mieanda@users.noreply.github.com>
2019-11-27 15:00:55 +0100

    Add files via upload

--- App.js
+++ App.js
@@ -8,6 +8,8 @@ import TP from './temp_kartor/TP.png';
 import TP4 from './temp_kartor/TP41.png';
 import TP5 from './temp_kartor/TP51.png';
 
+import pin from './symbols/pin.png';
+
 
 import K2 from './temp_kartor/K_2.svg';
 import T3 from './temp_kartor/TP_3.svg';
@@ -23,10 +25,11 @@ function App() {
   	<Router>
     <div>
  		<Switch>
-          <Route path="/Täppan">                                                    
+          <Route exact path="/Täppan">
             <Tappan />
           </Route>
-          <Route path="/Kåkenhus">   
+          <Route path="/Täppan/:floor/:room/:x/:y" component={Tappan}/>                                                    
+          <Route path="/Kåken">   
             <Kakenhus />
           </Route>
           <Route exact path="/">
@@ -88,7 +91,7 @@ var foundRooms = [];
 //varje rad i dropdown  + props.data.house
 function RoomInfo(props){
   return (
-    <Link className = "aa" to = {"/Täppan" }>
+    <Link className = "aa" to = {"/"+ props.data.house + "/" +  props.data.floor + "/" + props.data.room + "/" + props.data.coord.x + "/" + props.data.coord.y}>
     <div className="listOverlay">
     {props.data.room}
     </div>
@@ -96,25 +99,55 @@ function RoomInfo(props){
   )
 }
 
-function Tappan() {
+function Tappan(props) {
 	let bottom_floor = 3;
 	let top_floor = 5;
 	let current_floor = bottom_floor;
 
   const [floor, setFloor] = useState(3);
-  console.log(floor);
+  
+
+  
 
 	//Kallar på egen function för knappar samt bakgrund
 	if(current_floor == bottom_floor) {
-		return (
-			<div>
-			<Buttons floor={floor} setFloor={setFloor}/>
-			<div className = "background">
-				<img src={T3} className = "background_image"/>
-			</div>
+
+    if((props.match.params.room) === null){
+      console.log("hej");
+      return(
+        <div>
+          <Buttons floor={floor} setFloor={setFloor}/>
+          <div className = "background">
+            <img src={T3} className = "background_image"/> 
+          </div>
+        </div>
+
+        );
+    }
+    else{
+
+      return (
+        <div>
+         <Buttons floor={floor} setFloor={setFloor}/>
+        <div className = "background">
+           <img src={T3} className = "background_image"/>
+          
+          <div 
+        style={{
+          position: "absolute",
+          left: props.match.params.x +"px",
+          top: props.match.params.y +"px"
+        }}>
+        <img src={pin} className="pin"/>
+        </div>
+       </div>
     </div>
-  	);
+    );
+    }
+		
+  	
 	}
+
 
 	else if(current_floor == 4) {
 		return (
