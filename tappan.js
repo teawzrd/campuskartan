c12cab4a67cb3d40e459539471a9bf5dd5e87a26
Max <55145161+max98s@users.noreply.github.com>
2019-12-04 19:02:38 +0100

    Jonathans

---
+++ tappan.js
@@ -0,0 +1,39 @@
+import React,{useState} from 'react';
+import './App.css';
+import {BrowserRouter as Router, Switch, Route, useParams, Link } from "react-router-dom";
+import Buttons from './buttons.js';
+import TopBar from './topbar.js';
+import SearchBar from './searchbar.js';
+import SearchPin from './searchpin.js';
+import up_button from './symbols/up_arrow.png';
+import down_button from './symbols/down_white.svg';
+import upButton from './symbols/up_white.svg';
+
+
+const Tappan = props => 
+{        // ersätt "floor" med "props.floorTest" <---- ???? va??
+
+	let {floor, room, x, y} = useParams();
+
+	return(
+        <div>
+
+        	<TopBar building={"Täppan"} floorFromURL={floor}/>
+        	<SearchBar />	
+
+
+			<div className="background">
+
+				<img src={require('./maps/TP_' + floor + '.svg')} className="background_image"/>
+				<SearchPin />
+			</div>
+
+
+			<br/><br/>
+			You are now on floor: {floor}
+	    </div>
+	  
+	);
+}
+
+export default Tappan;
