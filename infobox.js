90854bf68bb2c58f56258d45458d1bef7bdf30cd
Henry Ljung <49097617+epikray@users.noreply.github.com>
2019-12-05 14:23:52 +0100

    Add files via upload

--- infobox.js
+++ infobox.js
@@ -1,8 +1,11 @@
 import React from 'react';
 import './App.css';
-import upButton from './symbols/up.png';
-import downButton from './symbols/down.png';
+import upButton from './symbols/up_white.svg';
+import downButton from './symbols/down_white.svg';
+import bUpButton from './symbols/up_grey.svg';
+import bDownButton from './symbols/down_grey.svg';
 import homeButton from './symbols/home.svg'; 
+import pin from './symbols/pin.svg';
 
 
 
@@ -12,9 +15,10 @@ const InfoBox = () => {
    return (
     <div className='infoBox'>
       <div>
-        <p><img src={homeButton} alt={'homeButton'} style={{width: '50px'}}/>Return to overview</p>
+        <img src={homeButton} alt={'homeButton'} style={{width: '50px'}}/><h2>Gå till övervyn</h2>
         <p><img src={upButton} alt={'upButton'} style={{width: '50px'}}/>
-           <img src={downButton} alt={'downButton'} style={{width: '50px'}}/>Change between floors</p>
+           <img src={downButton} alt={'downButton'} style={{width: '50px'}}/>Byt mellan våningar</p>
+        <p><img src={pin} alt={'pin'} style={{width: '50px'}}/>Eftersökta rum</p>
       </div>
     </div>
   );
